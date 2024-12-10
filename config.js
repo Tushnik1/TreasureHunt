8098078203b6070a86f3813d94c937ba1e66756f
Tushnik Dutta <139050107+Tushnik1@users.noreply.github.com>
2024-12-11 02:01:22 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+export const sysConfig = {
+    // frontend to backend link
+    FRONTEND : {
+        CLIENT_PORT : 5000,
+        SERVER_Q_FETCH : '',
+        SERVER_A_SUBMIT: '',
+        SERVER_CRED_CHECK: ''
+    },
+    // backend to database link
+    BACKEND : {
+        SERVER_PORT : 3000,
+        DATABASE_CON: '',
+        DATABASE_NAME: '',
+        COLLECTION_NAME: '',
+    }
+}
